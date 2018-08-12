906855c9eeafe4a6f0c58a89de43f80b7a185bcd
A0000524 <ecd.yangj@haier.com>
2018-08-12 20:55:42 +0800

    20190812

---
+++ src/views/ZFirst/ZHome1.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export const ZHome1 = (props) => {
+
+    return (
+        <div className="visual-item w1200" data-mode="fh-hot" data-purebox="adv" data-li="1"
+            data-diff="1" style={{ "display": "block" }}>
+
+            <div className="view">
+
+                <div className="index-festival-adv">
+                    <ul data-type="range" id="fh-hot_1" data-lift="年货专区">
+                        <li className="festival-adv-top">
+                            <a href="#" target="_blank">
+                                <img src="https://test.dscmall.cn/data/gallery_album/61/original_img/1515957501674637307.png" />
+                            </a>
+                        </li>
+                        <li className="festival-adv-bot">
+                            <a href="https://test.dscmall.cn/category.php?id=1129" target="_blank"><img
+                                src="https://test.dscmall.cn/data/gallery_album/61/original_img/1515953434094179058.png" /></a>
+                            <a href="https://test.dscmall.cn/category.php?id=1105" target="_blank"><img
+                                src="https://test.dscmall.cn/data/gallery_album/61/original_img/1515953435023649761.png" /></a>
+                            <a href="https://test.dscmall.cn/category.php?id=1109" target="_blank"><img
+                                src="https://test.dscmall.cn/data/gallery_album/61/original_img/1515953435777799300.png" /></a>
+                            <a href="https://test.dscmall.cn/category.php?id=33" target="_blank"><img
+                                src="https://test.dscmall.cn/data/gallery_album/61/original_img/1515953435162544293.png" /></a>
+                            <a href="https://test.dscmall.cn/category.php?id=12" target="_blank"><img
+                                src="https://test.dscmall.cn/data/gallery_album/61/original_img/1515953435545165923.png" /></a>
+                        </li>
+                    </ul>
+                    <div className="spec" data-spec=""></div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/views/ZFirst/ZHome2.jsx
@@ -0,0 +1,154 @@
+import React from 'react';
+
+export const ZHome2 = (props) => {
+
+    return (
+        <div className="visual-item w1200" data-mode="h-seckill" data-purebox="homeAdv" data-li="1"
+            data-diff="0" style={{ "display": "block" }}>
+
+            <div className="view">
+                <div className="seckill-channel" id="h-seckill_0" data-type="range" data-lift="秒杀活动"
+                    data-seckillid="{&quot;1&quot;:&quot;2,3,5,37,39&quot;,&quot;2&quot;:&quot;12,13,15,16,40&quot;,&quot;3&quot;:&quot;17,18,19,20,21&quot;,&quot;4&quot;:&quot;36,35,33,32,31&quot;,&quot;5&quot;:&quot;6,7,8,9,10,57,56,55,54&quot;}">
+                    <div className="box-hd clearfix">
+                        <h3>优惠政策</h3>
+                        <div className="seckill-more"><a href="seckill.php" target="_blank">更多优惠政策<i
+                            className="iconfont icon-right"></i></a></div>
+                    </div>
+                    <div className="box-bd clearfix">
+                        <div className="tempWrap" style={{ "overflow": "hidden", "position": "relative", "width": "1190px" }}>
+                            <ul style={{ "width": "2618px", "position": "relative", "overflow": "hidden", "padding": "0px", "margin": "0px", "left": "-238px" }}>
+                                <li className="opacity_img clone" style={{ "float": "left", "width": "228px" }}>
+                                    <div className="p-img"><a href="seckill.php?id=35" target="_blank"><img
+                                        src="images/201703/thumb_img/0_thumb_G_1490218873837.jpg" /></a></div>
+                                    <div className="p-name"><a href="seckill.php?id=35" target="_blank"
+                                        title="Nike 耐克官方 PRO COOL COMP LONG-SLEEVE 男子训练紧身衣703088">Nike
+                    耐克官方 PRO COOL COMP LONG-SLEEVE 男子训练紧身衣703088</a></div>
+                                    <div className="p-price">
+                                        <span className="shop-price"><em>¥</em>541.00</span>
+                                        <span className="original-price"><em>¥</em>298.80</span>
+                                    </div>
+                                </li>
+                                <li className="opacity_img" style={{ "float": "left", "width": "228px" }}>
+                                    <div className="p-img"><a href="seckill.php?id=33" target="_blank"><img
+                                        src="images/201703/thumb_img/0_thumb_G_1490218800612.jpg" /></a></div>
+                                    <div className="p-name"><a href="seckill.php?id=33" target="_blank"
+                                        title="洽福 转椅轮子万向轮 老板椅办公椅子滑轮电脑椅配件滚轮脚轮轱辘 静音灵活不伤地板质保三年下单备注安装方式">洽福
+                    转椅轮子万向轮 老板椅办公椅子滑轮电脑椅配件滚轮脚轮轱辘 静音灵活不伤地板质保三年下单备注安装方式</a></div>
+                                    <div className="p-price">
+                                        <span className="shop-price"><em>¥</em>541.00</span>
+                                        <span className="original-price"><em>¥</em>28.56</span>
+                                    </div>
+                                </li>
+                                <li className="opacity_img" style={{ "float": "left", "width": "228px" }}>
+                                    <div className="p-img"><a href="seckill.php?id=32" target="_blank"><img
+                                        src="images/201703/thumb_img/0_thumb_G_1490218770962.jpg" /></a></div>
+                                    <div className="p-name"><a href="seckill.php?id=32" target="_blank"
+                                        title="哑铃男士钢制家用电镀哑铃杠铃25 40公斤 纯钢哑铃盒装 健身器材 纯钢哑铃杠铃送哑铃架">哑铃男士钢制家用电镀哑铃杠铃25
+                    40公斤 纯钢哑铃盒装 健身器材 纯钢哑铃杠铃送哑铃架</a></div>
+                                    <div className="p-price">
+                                        <span className="shop-price"><em>¥</em>541.00</span>
+                                        <span className="original-price"><em>¥</em>432.00</span>
+                                    </div>
+                                </li>
+                                <li className="opacity_img" style={{ "float": "left", "width": "228px" }}>
+                                    <div className="p-img"><a href="seckill.php?id=31" target="_blank"><img
+                                        src="images/201703/thumb_img/0_thumb_G_1490218732576.jpg" /></a></div>
+                                    <div className="p-name"><a href="seckill.php?id=31" target="_blank"
+                                        title="T1多功能健身器材家用仰卧板收腹机哑铃凳仰卧起坐板折叠 健身椅 进口格子皮八档调节 承重500斤">T1多功能健身器材家用仰卧板收腹机哑铃凳仰卧起坐板折叠
+                    健身椅 进口格子皮八档调节 承重500斤</a></div>
+                                    <div className="p-price">
+                                        <span className="shop-price"><em>¥</em>541.00</span>
+                                        <span className="original-price"><em>¥</em>312.00</span>
+                                    </div>
+                                </li>
+                                <li className="opacity_img" style={{ "float": "left", "width": "228px" }}>
+                                    <div className="p-img"><a href="seckill.php?id=36" target="_blank"><img
+                                        src="images/201703/thumb_img/0_thumb_G_1490218905472.jpg" /></a></div>
+                                    <div className="p-name"><a href="seckill.php?id=36" target="_blank"
+                                        title="专业瑜伽服双线美背瑜伽背心吊带含胸垫一体式弹力修身运动健身服">专业瑜伽服双线美背瑜伽背心吊带含胸垫一体式弹力修身运动健身服</a>
+                                    </div>
+                                    <div className="p-price">
+                                        <span className="shop-price"><em>¥</em>541.00</span>
+                                        <span className="original-price"><em>¥</em>190.79</span>
+                                    </div>
+                                </li>
+                                <li className="opacity_img" style={{ "float": "left", "width": "228px" }}>
+                                    <div className="p-img"><a href="seckill.php?id=35" target="_blank"><img
+                                        src="images/201703/thumb_img/0_thumb_G_1490218873837.jpg" /></a></div>
+                                    <div className="p-name"><a href="seckill.php?id=35" target="_blank"
+                                        title="Nike 耐克官方 PRO COOL COMP LONG-SLEEVE 男子训练紧身衣703088">Nike
+                    耐克官方 PRO COOL COMP LONG-SLEEVE 男子训练紧身衣703088</a></div>
+                                    <div className="p-price">
+                                        <span className="shop-price"><em>¥</em>541.00</span>
+                                        <span className="original-price"><em>¥</em>298.80</span>
+                                    </div>
+                                </li>
+                                <li className="opacity_img clone" style={{ "float": "left", "width": "228px" }}>
+                                    <div className="p-img"><a href="seckill.php?id=33" target="_blank"><img
+                                        src="images/201703/thumb_img/0_thumb_G_1490218800612.jpg" /></a></div>
+                                    <div className="p-name"><a href="seckill.php?id=33" target="_blank"
+                                        title="洽福 转椅轮子万向轮 老板椅办公椅子滑轮电脑椅配件滚轮脚轮轱辘 静音灵活不伤地板质保三年下单备注安装方式">洽福
+                    转椅轮子万向轮 老板椅办公椅子滑轮电脑椅配件滚轮脚轮轱辘 静音灵活不伤地板质保三年下单备注安装方式</a></div>
+                                    <div className="p-price">
+                                        <span className="shop-price"><em>¥</em>541.00</span>
+                                        <span className="original-price"><em>¥</em>28.56</span>
+                                    </div>
+                                </li>
+                                <li className="opacity_img clone" style={{ "float": "left", "width": "228px" }}>
+                                    <div className="p-img"><a href="seckill.php?id=32" target="_blank"><img
+                                        src="images/201703/thumb_img/0_thumb_G_1490218770962.jpg" /></a></div>
+                                    <div className="p-name"><a href="seckill.php?id=32" target="_blank"
+                                        title="哑铃男士钢制家用电镀哑铃杠铃25 40公斤 纯钢哑铃盒装 健身器材 纯钢哑铃杠铃送哑铃架">哑铃男士钢制家用电镀哑铃杠铃25
+                    40公斤 纯钢哑铃盒装 健身器材 纯钢哑铃杠铃送哑铃架</a></div>
+                                    <div className="p-price">
+                                        <span className="shop-price"><em>¥</em>541.00</span>
+                                        <span className="original-price"><em>¥</em>432.00</span>
+                                    </div>
+                                </li>
+                                <li className="opacity_img clone" style={{ "float": "left", "width": "228px" }}>
+                                    <div className="p-img"><a href="seckill.php?id=31" target="_blank"><img
+                                        src="images/201703/thumb_img/0_thumb_G_1490218732576.jpg" /></a></div>
+                                    <div className="p-name"><a href="seckill.php?id=31" target="_blank"
+                                        title="T1多功能健身器材家用仰卧板收腹机哑铃凳仰卧起坐板折叠 健身椅 进口格子皮八档调节 承重500斤">T1多功能健身器材家用仰卧板收腹机哑铃凳仰卧起坐板折叠
+                    健身椅 进口格子皮八档调节 承重500斤</a></div>
+                                    <div className="p-price">
+                                        <span className="shop-price"><em>¥</em>541.00</span>
+                                        <span className="original-price"><em>¥</em>312.00</span>
+                                    </div>
+                                </li>
+                                <li className="opacity_img clone" style={{ "float": "left", "width": "228px" }}>
+                                    <div className="p-img"><a href="seckill.php?id=36" target="_blank"><img
+                                        src="images/201703/thumb_img/0_thumb_G_1490218905472.jpg" /></a></div>
+                                    <div className="p-name"><a href="seckill.php?id=36" target="_blank"
+                                        title="专业瑜伽服双线美背瑜伽背心吊带含胸垫一体式弹力修身运动健身服">专业瑜伽服双线美背瑜伽背心吊带含胸垫一体式弹力修身运动健身服</a>
+                                    </div>
+                                    <div className="p-price">
+                                        <span className="shop-price"><em>¥</em>541.00</span>
+                                        <span className="original-price"><em>¥</em>190.79</span>
+                                    </div>
+                                </li>
+                                <li className="opacity_img clone" style={{ "float": "left", "width": "228px" }}>
+                                    <div className="p-img"><a href="seckill.php?id=35" target="_blank"><img
+                                        src="images/201703/thumb_img/0_thumb_G_1490218873837.jpg" /></a></div>
+                                    <div className="p-name"><a href="seckill.php?id=35" target="_blank"
+                                        title="Nike 耐克官方 PRO COOL COMP LONG-SLEEVE 男子训练紧身衣703088">Nike
+                    耐克官方 PRO COOL COMP LONG-SLEEVE 男子训练紧身衣703088</a></div>
+                                    <div className="p-price">
+                                        <span className="shop-price"><em>¥</em>541.00</span>
+                                        <span className="original-price"><em>¥</em>298.80</span>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <a href="javascript:void(0);" className="prev"><i className="iconfont icon-left"></i></a>
+                        <a href="javascript:void(0);" className="next"><i className="iconfont icon-right"></i></a>
+                        <input type="hidden" name="sec_begin_time" value="" />
+                        <input type="hidden" name="sec_end_time" value="2018-07-17 16:00:00" />
+
+                    </div>
+                    <div className="spec" data-spec="" data-title="undefined"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
